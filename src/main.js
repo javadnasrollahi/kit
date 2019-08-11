5e835420351e6b7d828b2215a53635b3d0504974
javadnasrollahi <javadnasrollahi68@gmail.com>
2019-08-11 12:28:41 +0430

    بهط

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import App from './App'
 import router from './router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import '@/assets/css/fonts.css'
 
 Vue.config.productionTip = false
 Vue.use(vuex)
